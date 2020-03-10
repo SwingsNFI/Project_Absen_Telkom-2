a2feca68cadf98fb64d6aa82ead6e1b40b7d7cb2
SwingsNFI <59271780+SwingsNFI@users.noreply.github.com>
2020-03-10 21:20:51 +0700

    Add files via upload

---
+++ ProjectAbsenTelkom2/Backend/absenproject/absentelkom/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Tbl_siswa)
+admin.site.register(Tbl_guru)
+admin.site.register(Tbl_kelas)
+admin.site.register(Tbl_validasi)
+admin.site.register(Tbl_absen)
+admin.site.register(Tbl_acara)
+admin.site.register(Tbl_kategori)
+admin.site.register(Tbl_absen_ami)
